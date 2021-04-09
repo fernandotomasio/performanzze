8d3bdeba926552684d1cb633270c6a498b383647
fernandotomasio <fernandotomasio@gmail.com>
2021-04-09 20:16:08 -0300

    first commit

---
+++ src/main/java/br/com/jeel/performanzze/performanzze/PerformanzzeSoftwareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeel.performanzze.performanzze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanzzeSoftwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanzzeSoftwareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeel/performanzze/performanzze/PerformanzzeSoftwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeel.performanzze.performanzze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanzzeSoftwareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
